fef5a94265f6f9fec1a2739286d63d5f7c6e5f60
rdmUsername <55662864+rdmUsername@users.noreply.github.com>
2020-12-08 15:22:56 +0100

    Add files via upload

--- NewSiteProjetSQL/js/SearchFunction.js
+++ NewSiteProjetSQL/js/SearchFunction.js
@@ -4,4 +4,20 @@
     Date : 08.12.2020
     Desc : toute les fonction de la page search
 */
+let phonelist=[];
+function ShowPhones(listPhone){
+    let div=document.getElementById("PhoneResult");
+    listPhone.forEach(element => {
+        div.appendChild(element.slot)
+    });
+}
 
+function addPhoneSlot(name, imagesource, price, url){
+    phonelist.push(new PhoneSlot(name,imagesource,price,url));
+}
+function genPhoneListe(){
+    for (var i = 0; i < 40; i++) {
+        phonelist.push(new PhoneSlot("Phone Name n°"+phonelist.length,"../oder/Image/HuaweiP40.png",phonelist.length*10,""))
+    }
+    ShowPhones(phonelist);
+}
